539eaaf61b20750caf8e5f1fc1198c9908a1ff1f
Divyansh Taksalia <divyanshtaksalia2004@gmail.com>
2025-11-28 23:12:56 +0530

    Update button to include link for reading book

--- public/books.js
+++ public/books.js
@@ -291,7 +291,7 @@ function renderBooks(books, showAdminTools) {
                     <button data-id="${book.book_id}" class="btn-order" ${book.display_status !== 'available' ? 'disabled' : ''} data-status="${book.display_status}">
                         ${book.display_status === 'available' ? '📖 Issue Now' : (book.display_status === 'pending_issue' ? 'Request Sent' : 'Currently Issued')}
                     </button>
-                    <button data-id="${book.book_id}" class="open">
+                    <button data-id="${book.book_id}" class="open" href="https://cdn.ttgtmedia.com/rms/pdf/pragmatic_programmer_ch4.pdf">
                         Read
                     </button>
                 `} 
